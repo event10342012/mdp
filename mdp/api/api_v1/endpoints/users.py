e9d32de5769b10f595e5ec330ec80c1c086c62b5
leochen <event10342012@gmail.com>
2022-01-06 22:22:59 +0800

    fix: adjust

--- mdp/api/api_v1/endpoints/users.py
+++ mdp/api/api_v1/endpoints/users.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import Session
 
 from mdp import crud, models, schemas
 from mdp.api import deps
-from mdp.core.config import settings
 
 router = APIRouter()
 
@@ -36,7 +35,7 @@ def create_user(
     """
     Create new user.
     """
-    user = crud.user.get_by_email(db, email=user_in.email)
+    user = crud.user.get_by_username(db, username=user_in.username)
     if user:
         raise HTTPException(
             status_code=400,
@@ -51,7 +50,8 @@ def update_user_me(
         *,
         db: Session = Depends(deps.get_db),
         password: str = Body(None),
-        full_name: str = Body(None),
+        last_name: str = Body(None),
+        first_name: str = Body(None),
         email: EmailStr = Body(None),
         current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
@@ -62,8 +62,10 @@ def update_user_me(
     user_in = schemas.UserUpdate(**current_user_data)
     if password is not None:
         user_in.password = password
-    if full_name is not None:
-        user_in.full_name = full_name
+    if last_name is not None:
+        user_in.last_name = last_name
+    if first_name is not None:
+        user_in.first_name = first_name
     if email is not None:
         user_in.email = email
     user = crud.user.update(db, db_obj=current_user, obj_in=user_in)
@@ -79,33 +81,6 @@ def read_user_me(
     Get current user.
     """
     return current_user
-
-
-@router.post("/open", response_model=schemas.User)
-def create_user_open(
-        *,
-        db: Session = Depends(deps.get_db),
-        password: str = Body(...),
-        email: EmailStr = Body(...),
-        full_name: str = Body(None),
-) -> Any:
-    """
-    Create new user without the need to be logged in.
-    """
-    if not settings.USERS_OPEN_REGISTRATION:
-        raise HTTPException(
-            status_code=403,
-            detail="Open user registration is forbidden on this server",
-        )
-    user = crud.user.get_by_email(db, email=email)
-    if user:
-        raise HTTPException(
-            status_code=400,
-            detail="The user with this username already exists in the system",
-        )
-    user_in = schemas.UserCreate(password=password, email=email, full_name=full_name)
-    user = crud.user.create(db, obj_in=user_in)
-    return user
 
 
 @router.get("/{user_id}", response_model=schemas.User)
